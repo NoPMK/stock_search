fff14c9532baab41382a26820051c83fb1fa8609
Daniel, Bus <fullstackbus@gmail.com>
2024-12-11 13:04:49 +0100

    adjust navbar, add routing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,35 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const routes = [
+  {
+    name: "Search",
+    path: "/",
+  },
+  {
+    name: "Favorites",
+    path: "/favorites",
+  },
+];
+
 const Navbar = () => {
+  const router = useRouter();
+
   return (
-    <nav className="w-full max-w-full h-10">
-      <div className="flex justify-between pt-4">
-        <span className="pl-10">Logo</span>
-        <div className="flex gap-2 pr-10">
-          <span>Search</span>
-          <span>Favorites</span>
+    <nav className="w-full max-w-full h-16">
+      <div className="flex justify-between p-4">
+        <span className="p-4">Logo</span>
+        <div className="flex gap-2">
+          {routes.map((route) => (
+            <span
+              key={route.name}
+              onClick={() => router.push(route.path)}
+              className="flex items-center justify-center cursor-pointer px-4 rounded-md bg-slate-800 hover:bg-slate-700"
+            >
+              {route.name}
+            </span>
+          ))}
         </div>
       </div>
     </nav>
