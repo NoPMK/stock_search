82a8cda186c47da19a6c27de7d9d52fe09ff4d01
Daniel, Bus <fullstackbus@gmail.com>
2024-12-11 10:41:18 +0100

    adjust navbar placement

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="container">
+    <nav className="w-full max-w-full h-10">
       <div className="flex justify-between pt-4">
         <span className="pl-10">Logo</span>
         <div className="flex gap-2 pr-10">
