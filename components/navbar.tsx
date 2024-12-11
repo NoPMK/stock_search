293fa0bd840ea25550b1db7eb0b1e64216cceff9
Daniel, Bus <fullstackbus@gmail.com>
2024-12-11 14:36:03 +0100

    remove unnecessary item

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,34 +2,20 @@
 
 import { useRouter } from "next/navigation";
 
-const routes = [
-  {
-    name: "Search",
-    path: "/",
-  },
-  {
-    name: "Favorites",
-    path: "/favorites",
-  },
-];
-
 const Navbar = () => {
   const router = useRouter();
 
   return (
-    <nav className="w-full max-w-full h-22 fixed bg-slate-900">
+    <nav className="w-full max-w-full h-22 fixed bg-slate-900 z-[1000]">
       <div className="flex justify-between p-4">
         <span className="p-4">Logo</span>
         <div className="flex gap-2">
-          {routes.map((route) => (
-            <span
-              key={route.name}
-              onClick={() => router.push(route.path)}
-              className="flex items-center justify-center cursor-pointer px-4 rounded-md bg-slate-800 hover:bg-slate-700"
-            >
-              {route.name}
-            </span>
-          ))}
+          <span
+            onClick={() => router.push("/")}
+            className="flex items-center justify-center cursor-pointer px-4 rounded-md bg-slate-800 hover:bg-slate-700"
+          >
+            Search
+          </span>
         </div>
       </div>
     </nav>
