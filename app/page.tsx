34fb2232ec668c0806fc4d409acdb0df486f154e
Daniel, Bus <fullstackbus@gmail.com>
2024-12-11 13:04:33 +0100

    adjust homepage to use components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
+"use client";
+
 import Searchbar from "@/components/searchbar";
+import SearchList from "@/components/searchlist";
+import { Stock } from "@/data/stock";
+import { stocks } from "@/data/stock-info";
+import { useState } from "react";
 
 export default function Home() {
+  const [filteredStocks, setFilteredStocks] = useState<Stock[]>(stocks);
+
+  const handleSearch = (query: string) => {
+    if (!query) {
+      setFilteredStocks(stocks);
+      return;
+    }
+
+    const filteredStocks = stocks.filter(
+      (stock) =>
+        stock.name.toLowerCase().includes(query) ||
+        stock.symbol.toLowerCase().includes(query)
+    );
+
+    setFilteredStocks(filteredStocks);
+  };
+
   return (
     <div className="flex min-w-full justify-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
       {" "}
-      <main className="flex w-full justify-center">
-        <Searchbar />
+      <main className="flex flex-col w-full justify-center items-center">
+        <Searchbar onSearch={handleSearch} />
+        <SearchList stocks={filteredStocks} />
       </main>
     </div>
   );
