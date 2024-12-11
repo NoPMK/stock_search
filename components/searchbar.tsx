ff0aeaab85ea0b53082f096e4ec9c8221290a2fe
Daniel, Bus <fullstackbus@gmail.com>
2024-12-11 13:03:00 +0100

    modify searchbar component

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,27 +1,12 @@
-"use client";
-
-import { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
-import { stocks } from "../data/stock-info";
-import { Stock } from "@/data/stock";
 
-export default function Searchbar() {
-  const [activeSearch, setActiveSearch] = useState<Stock[]>([]);
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
 
+export default function Searchbar({ onSearch }: SearchBarProps) {
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const query = e.target.value.toLowerCase().trim();
-    if (!query) {
-      setActiveSearch([]);
-      return;
-    }
-
-    const filteredStocks = stocks.filter(
-      (stock) =>
-        stock.name.toLowerCase().includes(query) ||
-        stock.symbol.toLowerCase().includes(query)
-    );
-
-    setActiveSearch(filteredStocks.slice(0, 5));
+    onSearch(e.target.value.toLowerCase().trim());
   };
 
   return (
@@ -40,19 +25,6 @@ export default function Searchbar() {
           <AiOutlineSearch />
         </button>
       </div>
-
-      {activeSearch.length > 0 && (
-        <div
-          className="absolute top-20 p-4 bg-slate-800 text-white w-full 
-          rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2"
-        >
-          {activeSearch.map((s, index) => (
-            <span key={index}>
-              {s.name} - {s.symbol}
-            </span>
-          ))}
-        </div>
-      )}
     </form>
   );
 }
