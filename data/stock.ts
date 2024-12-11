9b9170622c0942d181847852957fa5c754285964
Daniel, Bus <fullstackbus@gmail.com>
2024-12-11 13:02:14 +0100

    clean data

--- data/stock.ts
+++ data/stock.ts
@@ -3,9 +3,5 @@ export interface Stock {
   name: string;
   type: string;
   region: string;
-  marketOpen: string;
-  marketClose: string;
-  timezone: string;
   currency: string;
-  matchScore: string;
 }
